5ff8e2505aacfb9107d2c41980374385dc0200ba
graceon <graceon@163.com>
2020-05-19 21:41:39 +0800

    day3

--- libs/configs/cfgs.py
+++ libs/configs/cfgs.py
@@ -5,13 +5,60 @@ import tensorflow as tf
 import math
 
 """
-v3 + iou-smooth l1 loss
-
-
+v1 + two refine stages
+This is your result for task 1:
+    mAP: 0.6729130998778783
+    ap of each class: plane:0.8826149967762923,
+    baseball-diamond:0.7342284978013562,
+    bridge:0.4514498941155807,
+    ground-track-field:0.672091907176201,
+    small-vehicle:0.6709474430972782,
+    large-vehicle:0.7369526299825417,
+    ship:0.7021106544077973,
+    tennis-court:0.9064314756695351,
+    basketball-court:0.7657412068215571,
+    storage-tank:0.7984134345038421,
+    soccer-ball-field:0.5386417366552334,
+    roundabout:0.6093094275405536,
+    harbor:0.5695698766743563,
+    swimming-pool:0.6065833385927675,
+    helicopter:0.4486099783532847
+The submitted information is :
+Description: RetinaNet_DOTA_R3Det_2x_20191118_108w
+Username: yangxue
+Institute: DetectionTeamUCAS
+Emailadress: yangxue16@mails.ucas.ac.cn
+TeamMembers: yangxue, yangjirui
+-----------------------------------------------------------------
+merge all refine stages results
+This is your result for task 1:
+    mAP: 0.6765553919838632
+    ap of each class:
+    plane:0.8842026956804583,
+    baseball-diamond:0.7385818759437492,
+    bridge:0.4508422918807599,
+    ground-track-field:0.6795929425489206,
+    small-vehicle:0.674452811091998,
+    large-vehicle:0.7398138561006364,
+    ship:0.7026655772347278,
+    tennis-court:0.9070912170899791,
+    basketball-court:0.7776849884962305,
+    storage-tank:0.8044493538968289,
+    soccer-ball-field:0.5477872349765581,
+    roundabout:0.6053087523424203,
+    harbor:0.5729741901030716,
+    swimming-pool:0.6047761287319094,
+    helicopter:0.45810696363969794
+The submitted information is :
+Description: RetinaNet_DOTA_R3Det_2x_20191118_merge_108w
+Username: yangxue
+Institute: DetectionTeamUCAS
+Emailadress: yangxue16@mails.ucas.ac.cn
+TeamMembers: yangxue, yangjirui
 """
 
 # ------------------------------------------------
-VERSION = 'RetinaNet_DOTA_R3Det_plusplus_2x_20200502'
+VERSION = 'RetinaNet_DOTA_R3Det_2x_20191118'
 NET_NAME = 'resnet50_v1d'  # 'MobilenetV2'
 ADD_BOX_IN_TENSORBOARD = True
 
@@ -19,13 +66,11 @@ ADD_BOX_IN_TENSORBOARD = True
 ROOT_PATH = os.path.abspath('../')
 print(20*"++--")
 print(ROOT_PATH)
-# GPU_GROUP = "0,1,2,3"
-GPU_GROUP = "0"
+GPU_GROUP = "1,3"
 NUM_GPU = len(GPU_GROUP.strip().split(','))
 SHOW_TRAIN_INFO_INTE = 20
 SMRY_ITER = 200
-# SAVE_WEIGHTS_INTE = 27000 * 2
-SAVE_WEIGHTS_INTE = 1000
+SAVE_WEIGHTS_INTE = 27000 * 2
 
 SUMMARY_PATH = ROOT_PATH + '/output/summary'
 TEST_SAVE_PATH = ROOT_PATH + '/tools/test_result'
@@ -52,7 +97,7 @@ GRADIENT_CLIPPING_BY_NORM = 10.0  # if None, will not clip
 
 CLS_WEIGHT = 1.0
 REG_WEIGHT = 1.0
-USE_IOU_FACTOR = True
+USE_IOU_FACTOR = False
 
 BATCH_SIZE = 1
 EPSILON = 1e-5
@@ -85,7 +130,7 @@ FINAL_CONV_BIAS_INITIALIZER = tf.constant_initializer(value=-math.log((1.0 - PRO
 WEIGHT_DECAY = 1e-4
 USE_GN = False
 NUM_SUBNET_CONV = 4
-NUM_REFINE_STAGE = 1
+NUM_REFINE_STAGE = 2
 USE_RELU = False
 FPN_CHANNEL = 256
 
@@ -117,12 +162,12 @@ FILTERED_SCORE = 0.05
 VIS_SCORE = 0.4
 
 # --------------------------------------------MASK config
-USE_SUPERVISED_MASK = True
+USE_SUPERVISED_MASK = False
 MASK_TYPE = 'r'  # r or h
 BINARY_MASK = False
 SIGMOID_ON_DOT = False
 MASK_ACT_FET = True  # weather use mask generate 256 channels to dot feat.
 GENERATE_MASK_LIST = ["P3", "P4", "P5", "P6", "P7"]
-ADDITION_LAYERS = [1, 1, 1, 1, 1]  # add 4 layer to generate P2_mask, 2 layer to generate P3_mask
+ADDITION_LAYERS = [4, 4, 3, 2, 2]  # add 4 layer to generate P2_mask, 2 layer to generate P3_mask
 ENLAEGE_RF_LIST = ["P3", "P4", "P5", "P6", "P7"]
 SUPERVISED_MASK_LOSS_WEIGHT = 1.0
--- step.md
+++ step.md
@@ -21,8 +21,38 @@ python multi_gpu_train_r3det.py
 
 python test_dota_r3det.py --test_dir='/fdisk/DOTA/val/images/' --gpus=0
 
+python demo_dota_r3det.py --test_dir='/fdisk/DOTA/val/images/' --gpus=0
+
 python test_dota_r3det.py --test_dir='/fdisk/DOTA/smalltest/' --gpus=0
+
+python demo_dota_r3det.py --test_dir='/fdisk/DOTA/smalltest/' --gpus=0
+
 
 export CUDA_HOME=/usr/local/cuda
 export PATH=/usr/local/cuda/bin:$PATH
 export LD_LIBRARY_PATH=/usr/local/cuda/lib64:"$LD_LIBRARY_PATH:/usr/local/cuda/lib64:/usr/local/cuda/extras/CUPTI/lib64"
+
+
+cfg.py
+CLASS_NUM = 16
+label_dict.py
+cfgs.DATASET_NAME.startswith('DOTA'):
+    NAME_LABEL_MAP = {
+        'back_ground': 0,
+        'roundabout': 1,
+        'tennis-court': 2,
+        'swimming-pool': 3,
+        'storage-tank': 4,
+        'soccer-ball-field': 5,
+        'small-vehicle': 6,
+        'ship': 7,
+        'plane': 8,
+        'large-vehicle': 9,
+        'helicopter': 10,
+        'harbor': 11,
+        'ground-track-field': 12,
+        'bridge': 13,
+        'basketball-court': 14,
+        'baseball-diamond': 15,
+        'container-crane':16
+    }
